6025e799a211604cde467b3f85bba8e45c4750ec
Unknown <mtouverey@methodinthemadness.eu>
2017-03-30 00:16:48 +0200

    style(BuildContext): made public props readonly

--- src/BuildContext.ts
+++ src/BuildContext.ts
@@ -2,15 +2,15 @@ import * as path from 'path';
 import { ProjectDirectory } from './unity_project';
 
 export class BuildContext {
-    public assetsDirName: string;
-    public assetsDir: string;
-    public assetBundleDir: string;
+    public readonly assetsDirName: string;
+    public readonly assetsDir: string;
+    public readonly assetBundleDir: string;
 
     private ctorTimestamp = new Date().getTime();
 
     public constructor() {
-        this.assetsDirName = `CopiedAssets-${this.ctorTimestamp}`;
-        this.assetsDir = path.resolve(`${ProjectDirectory}/Assets/${this.assetsDirName}`);
+        this.assetsDirName  = `CopiedAssets-${this.ctorTimestamp}`;
+        this.assetsDir      = path.resolve(`${ProjectDirectory}/Assets/${this.assetsDirName}`);
         this.assetBundleDir = path.resolve(`${ProjectDirectory}/GeneratedAssetBundles-${this.ctorTimestamp}`);
     }
 }
