b6b8a3ef78869c683aba4d541d457b4f53df780c
Unknown <mtouverey@methodinthemadness.eu>
2017-04-10 17:40:41 +0200

    feat(index): changed the way build targets are exported

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { AssetsBundler } from './AssetsBundler';
+import * as BuildTargets from './build_targets';
 import { ReadableFileInput } from './stream_maker';
 
 export * from './AssetsBundler';
-export * from './build_targets';
 export { setUnityPath } from './unity_invoker';
+export { BuildTargets };
 
 export function bundle(...files: ReadableFileInput[]): AssetsBundler {
     const bundler = new AssetsBundler();
