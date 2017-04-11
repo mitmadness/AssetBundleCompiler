ac4ffb445bf52036fbc559c2c2f1b039a34fdf43
Unknown <mtouverey@methodinthemadness.eu>
2017-04-11 16:44:06 +0200

    test(stream_maker): finished implenting tests

--- test/stream_maker.test.ts
+++ test/stream_maker.test.ts
@@ -1,15 +1,46 @@
 import { expect } from 'chai';
-import * as fs from 'fs';
+import * as fs from 'fs-extra';
+import * as os from 'os';
+import * as path from 'path';
 import * as streamMaker from '../src/stream_maker';
 
 describe('stream_maker', () => {
-    describe('#normalizeWriteStream()', () => {
+    const tmpFilePath = path.normalize(`${os.tmpdir()}/test.empty`);
+
+    describe('isWriteStream()', () => {
+        it('should recognize WriteStreams', () => {
+            const stream = fs.createWriteStream(tmpFilePath);
+            expect(streamMaker.isWriteStream(stream)).to.be.true;
+        });
+
+        it('should return false for non-WriteStreams', () => {
+            const stream = fs.createReadStream(tmpFilePath);
+            expect(streamMaker.isWriteStream(stream as any)).to.be.false;
+            expect(streamMaker.isWriteStream(null as any)).to.be.false;
+        });
+    });
+
+    describe('isReadStream()', () => {
+        it('should recognize ReadStreams', () => {
+            const stream = fs.createReadStream(tmpFilePath);
+            expect(streamMaker.isReadStream(stream)).to.be.true;
+        });
+
+        it('should return false for non-ReadStreams', () => {
+            const stream = fs.createWriteStream(tmpFilePath);
+            expect(streamMaker.isReadStream(stream as any)).to.be.false;
+            expect(streamMaker.isReadStream(null as any)).to.be.false;
+        });
+    });
+
+    describe('normalizeWriteStream()', () => {
         it('should convert paths to write streams', () => {
-            expect(streamMaker.normalizeWriteStream('test.empty')).to.have.property('bytesWritten');
+            const stream = streamMaker.normalizeWriteStream(tmpFilePath);
+            expect(streamMaker.isWriteStream(stream)).to.be.true;
         });
 
         it('should return write streams as-is', () => {
-            const stream = fs.createWriteStream('test.empty');
+            const stream = fs.createWriteStream(tmpFilePath);
             expect(streamMaker.normalizeWriteStream(stream)).to.equal(stream);
         });
 
@@ -19,4 +50,24 @@ describe('stream_maker', () => {
             expect(() => { streamMaker.normalizeWriteStream(String as any); }).to.throw();
         });
     });
+
+    describe('normalizeReadStream()', () => {
+        it('should convert paths to read streams', () => {
+            const stream = streamMaker.normalizeReadStream(tmpFilePath);
+            expect(streamMaker.isReadStream(stream)).to.be.true;
+        });
+
+        it('should return read streams as-is', () => {
+            const stream = fs.createReadStream(tmpFilePath);
+            expect(streamMaker.normalizeReadStream(stream)).to.equal(stream);
+        });
+
+        it('should throw when encountering something other than a string or read stream', () => {
+            expect(() => { streamMaker.normalizeReadStream(null); }).to.throw();
+            expect(() => { streamMaker.normalizeReadStream(42 as any); }).to.throw();
+            expect(() => { streamMaker.normalizeReadStream(String as any); }).to.throw();
+        });
+    });
+
+    after(() => fs.removeSync(tmpFilePath));
 });
