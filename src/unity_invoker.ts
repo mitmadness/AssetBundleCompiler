0f6de86b31a874c6b9aadb95a54f8a610adbf310
Unknown <mtouverey@methodinthemadness.eu>
2017-05-26 12:38:57 +0200

    feat(unity_invoker): handle unrecognized assets types errors
    Those were not making Unity's process crash. Now we intercept them and make the compilation fail with an exception.

--- src/unity_invoker.ts
+++ src/unity_invoker.ts
@@ -14,20 +14,50 @@ export async function generateAssetBundle(
     unityLogger: logger.SimpleLogger = logger.noopLogger,
     signalAssetProcessed: logger.SimpleLogger = logger.noopLogger
 ) {
+    const scriptOptions = {
+        cAssetNames, cAssetBundleDirectory, cAssetBundleName,
+        cAssetBundleBuildOptions: Array.from(cAssetBundleBuildOptions),
+        cAssetBundleTarget
+    };
+
+    let compilerError: Error|null = null;
+
+    function handleLogLine(message: string): void {
+        unityLogger(message);
+
+        const updatingAsset = getUpdatingAsset(message);
+        const unrecogAsset = getUnrecognizedAsset(message);
+
+        if (updatingAsset) {
+            signalAssetProcessed(updatingAsset);
+        } else if (unrecogAsset) {
+            compilerError = new Error(`File "${unrecogAsset}" is not processable by Unity, this is not a valid asset.`);
+        }
+    }
+
     await invokeHeadlessUnity()
         .projectPath(directory)
         .executeMethod('AssetBundleCompiler.Convert')
-        .withOptions({
-            cAssetNames, cAssetBundleDirectory, cAssetBundleName,
-            cAssetBundleBuildOptions: Array.from(cAssetBundleBuildOptions),
-            cAssetBundleTarget
-        })
-        .run((message) => {
-            unityLogger(message);
+        .withOptions(scriptOptions)
+        .run(handleLogLine);
 
-            const matches = message.match(/^Updating Assets\/CopiedAssets\/(.+?)(?= - GUID)/);
-            if (matches !== null) {
-                signalAssetProcessed(matches[1]);
-            }
-        });
+    //=> For now we rethrow the last encountered error, sometimes long after it has been encoutered.
+    //   @todo make it better.
+    if (compilerError) {
+        throw compilerError;
+    }
+}
+
+function getUpdatingAsset(message: string): string|null {
+    const updateMessage = /^Updating Assets\/CopiedAssets\/(.+?)(?= - GUID)/;
+    const matches = message.match(updateMessage);
+
+    return matches !== null ? matches[1] : null;
+}
+
+function getUnrecognizedAsset(message: string): string|null {
+    const unrecogMessage = /^Unrecognized assets cannot be included in AssetBundles?: "Assets\/CopiedAssets\/(.+?)"/;
+    const matches = message.match(unrecogMessage);
+
+    return matches !== null ? matches[1] : null;
 }
